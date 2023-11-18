ea5658513dd0340b46a7ffaa90d1de47401c64b8
Hirowncha Saravanaperumal <hirownchassp@Hirownchas-MacBook-Air.local>
2023-11-18 20:18:13 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Invoice from "./components/App";
+
+
+
+function App() {
+ 
+  return (
+    <>
+    <Invoice/>
+    
+    </>
+    
+  );
+}
+
+export default App;
